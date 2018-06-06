b9bd50d0e0f6a24866ece0c28b9be01c8b49fa89
FathiLakhdhar <fathi.lakdhar@yahoo.fr>
2018-06-06 15:31:29 +0100

    Add toggle wrapper OFF/ON

--- popup.js
+++ popup.js
@@ -1,45 +1,18 @@
-let changeColor = document.getElementById('changeColor');
-let start = document.getElementById('start');
-let stop = document.getElementById('stop');
+let checkbox = document.getElementById('checkbox');
 
-let isStarted = false;
-
-start.onclick = function (element) {  
-    console.log('start');
-    if(!isStarted){
-        isStarted = !isStarted;
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action : "start" }, function (response) {
-                console.log(response.farewell);
-            });
-        });
-    }
-};
-
-stop.onclick = function (element) {  
-    console.log('stop');
-    if(isStarted){
-        isStarted = !isStarted;
-        chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action : "stop" }, function (response) {
-                console.log(response.farewell);
-            });
-        });
-    }
-};
-
-
-///////////////////////////////////////////////////////////////////////////
-chrome.storage.sync.get('color', function (data) {
-    changeColor.style.backgroundColor = data.color;
-    changeColor.setAttribute('value', data.color);
+chrome.storage.local.get(['action'], function(data) {
+	checkbox.checked = (data.action === "start")?true:false;
 });
-changeColor.onclick = function (element) {
-    let color = element.target.value;
+
+checkbox.onchange = function(event){
+    event.preventDefault();
+    let action = (event.target.checked)?"start":"stop";
+	chrome.storage.local.set({action: action}, function() {
+   		console.log('action is set to ',action);
+    });
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        chrome.tabs.executeScript(
-            tabs[0].id,
-            { code: 'document.body.style.backgroundColor = "' + color + '";' }
-        );
+        chrome.tabs.sendMessage(tabs[0].id, { action : action }, function (response) {
+            console.log(response.farewell);
+        });
     });
 };
