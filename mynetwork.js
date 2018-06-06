b68f2b4af957295c1fe33108478c1715be4b74bc
FathiLakhdhar <fathi.lakdhar@yahoo.fr>
2018-06-06 14:02:24 +0100

    Content scripts

---
+++ mynetwork.js
@@ -0,0 +1,74 @@
+let api = (function () {
+    var stop = false;
+    var count = 0;
+    var singleton = null;
+    var time = Math.floor((Math.random() * 9000) + 9000);
+    var clientHeight = document.body.clientHeight;
+
+    function periodicall() {
+        if (stop) return;
+        if ((clientHeight * 1.5) > document.body.clientHeight) {
+            scrollDown();
+        }
+        time = Math.floor((Math.random() * 9000) + 9000);
+        sendRequest();
+        setTimeout(periodicall, time);
+    };
+
+    function sendRequest() {
+        var btn = document.querySelector("button.button-secondary-small");
+        if (btn) {
+            count++;
+            btn.click();
+            console.log('Clicked : ', btn.querySelector("span.visually-hidden").innerText.trim());
+        }
+    }
+    function scrollDown() {
+        scroll(0, 0);
+        scroll(0, document.body.clientHeight);
+        setTimeout(function () {
+            scroll(0, 0);
+        }, 1000)
+    }
+
+    return {
+        start: () => {
+            stop = false;
+            if (singleton == null) {
+                singleton = {};
+                singleton.periodicall = periodicall;
+                console.log("started...");
+                scrollDown();
+                singleton.periodicall();
+            } else {
+                console.log("is run")
+                return;
+            }
+        },
+        stop: () => {
+            stop = true;
+            singleton = null;
+            console.log("stoped...")
+            console.info("Nombre invitation : ", count)
+        },
+        count: () => count
+    }
+})();
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.action === "start"){
+            console.log(request);
+            sendResponse({ farewell: "start send invites" });
+            api.start();
+        }
+        if (request.action === "stop"){
+            console.log(request);
+            sendResponse({ farewell: "stop send invites" });
+            api.stop();
+        }
+        if (request.action === "count"){
+            console.log(request);
+            sendResponse({ farewell: `count ${api.count()}` });
+        }
+});
---
+++ searchPeople.js
@@ -0,0 +1,75 @@
+let api = (function(){
+
+    var count = 0;
+    var index = -1;
+    var stop = true;
+    function periodicall(){
+        var btnInvites = document.querySelectorAll(".search-result__actions button");
+        if(stop) return;
+        index++;
+        scroll(0, document.body.clientHeight * (index / 10));
+        if(index >= btnInvites.length){
+            index = 0;
+            console.info("load next page");
+            document.querySelector("button.next").click()
+            setTimeout(function(){
+                periodicall();
+                scroll(0, document.body.clientHeight);
+            }, 20000);
+        }else{
+            
+            console.log("index : ",index);        
+            if(btnInvites[index] && btnInvites[index].innerText == "Se connecter"){
+                console.log("before click")
+                btnInvites[index].click();
+                count++;
+                console.log("after click")
+    
+                setTimeout(function(){
+                    var envoyer = document.querySelector("div.send-invite__actions button.button-primary-large");
+                    if(envoyer.innerText == "Envoyer maintenant"){
+                        envoyer.click();
+                    }else{
+                        document.querySelector("button.send-invite__cancel-btn").click();
+                    }
+                    setTimeout(periodicall, 3000);
+    
+                }, 2000);
+            }else{
+                setTimeout(periodicall, 1000);
+            }
+        }
+
+        
+    };
+
+
+    return {
+        stop : ()=> stop = true,
+        start : ()=> {
+            if(stop){
+                stop = false;
+                periodicall();
+            }
+        },
+        count: ()=> count
+    } 
+})();
+
+chrome.runtime.onMessage.addListener(
+    function (request, sender, sendResponse) {
+        if (request.action === "start"){
+            console.log(request);
+            sendResponse({ farewell: "start send invites" });
+            api.start();
+        }
+        if (request.action === "stop"){
+            console.log(request);
+            sendResponse({ farewell: "stop send invites" });
+            api.stop();
+        }
+        if (request.action === "count"){
+            console.log(request);
+            sendResponse({ farewell: `count ${api.count()}` });
+        }
+});
