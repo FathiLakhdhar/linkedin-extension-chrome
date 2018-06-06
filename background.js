1ec726675f0e4859b0bc8f4acb66f31b6b9b9bb3
FathiLakhdhar <fathi.lakdhar@yahoo.fr>
2018-06-06 15:33:32 +0100

    Chrome: local storage onInstalled extension
    action : start OR stop

--- background.js
+++ background.js
@@ -1,4 +1,9 @@
 chrome.runtime.onInstalled.addListener(function () {
+
+	chrome.storage.local.set({action: "stop"}, function() {
+        console.log('action is set to stop');
+    });
+
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
@@ -8,5 +13,4 @@ chrome.runtime.onInstalled.addListener(function () {
             actions: [new chrome.declarativeContent.ShowPageAction()]
         }]);
     });
-
 });
